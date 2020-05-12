21bfd40c666341f9dd047f19fc261fd0749aa19c
Philippe DENIEL <philippe.deniel@cea.fr>
2020-05-12 09:23:20 +0000

    Conversion functions are messy
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- lhsmtool_phobos.c
+++ lhsmtool_phobos.c
@@ -716,6 +716,49 @@ static void bin2hexstr(const char *bin, size_t len, char *out)
         out[len*2] = '\0';
 }
 
+static int hexchr2bin(const char hex, char *out)
+{
+        if (out == NULL)
+                return 0;
+
+        if (hex >= '0' && hex <= '9') {
+                *out = hex - '0';
+        } else if (hex >= 'A' && hex <= 'F') {
+                *out = hex - 'A' + 10;
+        } else if (hex >= 'a' && hex <= 'f') {
+                *out = hex - 'a' + 10;
+        } else {
+                return 0;
+        }
+
+        return 1;
+}
+
+static size_t hexstr2bin(const char *hex, char *out)
+{
+        size_t len;
+        char   b1;
+        char   b2;
+        size_t i;
+
+        if (hex == NULL || *hex == '\0' || out == NULL)
+                return 0;
+
+        len = strlen(hex);
+        if (len % 2 != 0)
+                return 0;
+        len /= 2;
+
+        memset(out, 'A', len);
+        for (i=0; i<len; i++) {
+                if (!hexchr2bin(hex[i*2], &b1) || !hexchr2bin(hex[i*2+1], &b2)) {
+                        return 0;
+                }
+                out[i] = (b1 << 4) | b2;
+        }
+        return len;
+}
+
 
 /*
  * Copytool functions (with ct_ prefix)
@@ -1053,6 +1096,8 @@ static int ct_restore(const struct hsm_action_item *hai, const long hal_flags)
 	char				 dst[PATH_MAX];
 	char				 hexstripephobos[PATH_MAX];
 	char				 hexstripefs[PATH_MAX];
+	char				 lov_bufdbg[XATTR_SIZE_MAX];
+	size_t				 lov_sizedbg = sizeof(lov_bufdbg);
 	char				 lov_buf[XATTR_SIZE_MAX];
 	size_t				 lov_size = sizeof(lov_buf);
 	int				 rc;
@@ -1089,12 +1134,18 @@ static int ct_restore(const struct hsm_action_item *hai, const long hal_flags)
 		set_lovea = true;
 	}
 	bin2hexstr(lov_buf, lov_size, hexstripefs);
+	printf("i-----> hexstripe in FS = %s\n", hexstripefs); 
 
 	/* Get stripe from phobos */
 	rc = phobos_op_getstripe(&hai->hai_fid, hexstripephobos);
 	printf("i-----> phobos_op_getstripe: rc =%d\n", rc); 
 	printf("i-----> hexstripe in phobos = %s\n", hexstripephobos); 
 
+	lov_sizedbg = hexstr2bin(hexstripephobos, lov_bufdbg);
+	printf("----> lov_size=%d  lov_sizedbg=%d\n",
+	       (int)lov_size, (int)lov_sizedbg); 
+
+	/* start the restore operation */
 	rc = ct_begin_restore(&hcp, hai, mdt_index, open_flags);
 	if (rc < 0)
 		goto fini;
