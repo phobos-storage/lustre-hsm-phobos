79a413f5d3f720bc8b5c3dc25e9a417f2f265350
Patrice LUCAS <patrice.lucas@cea.fr>
2025-06-06 10:33:48 +0200

    phobos API: compatibility with Phobos 3 API
    Support phobos version 3.0.0 by adding alive objects scope to
    delete and get xfer params.
    
    Change-Id: I2e9e6b3771bf5c649a46d503313d37091a966647
    Signed-off-by: Patrice LUCAS <patrice.lucas@cea.fr>

--- lhsmtool_phobos.c
+++ lhsmtool_phobos.c
@@ -377,6 +377,7 @@ static int phobos_op_del(const struct lu_fid *fid, const struct buf *hints)
 
     /* DO THE DELETE */
     xfer.xd_op = PHO_XFER_OP_DEL;
+    xfer.xd_params.delete.scope = DSS_OBJ_ALIVE;
     xfer.xd_ntargets = 1;
     xfer.xd_targets = &xtgt;
     xtgt.xt_objid = strdup(obj);
@@ -583,6 +584,7 @@ static int phobos_op_get(const struct lu_fid *fid,
     }
 
     xfer.xd_op = PHO_XFER_OP_GET;
+    xfer.xd_params.get.scope = DSS_OBJ_ALIVE;
     xfer.xd_flags = 0;
     xfer.xd_ntargets = 1;
     xfer.xd_targets = &xtgt;
@@ -621,6 +623,7 @@ static int phobos_op_getlayout(const struct lu_fid *fid,
     }
 
     xfer.xd_op = PHO_XFER_OP_GETMD;
+    xfer.xd_params.get.scope = DSS_OBJ_ALIVE;
     xfer.xd_flags = 0;
     xfer.xd_ntargets = 1;
     xfer.xd_targets = &xtgt;
